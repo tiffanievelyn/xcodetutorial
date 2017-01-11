6fed91faa8a1d3a98d39c045c705ce80949c7000
Tiffani Evelyn <telim2@student.monash.edu>
2017-01-11 12:30:49 +1100

    Initial Commit

---
+++ Sample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sample
+//
+//  Created by Tiffani Evelyn on 11/01/2017.
+//  Copyright © 2017 Tiffani Evelyn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Tiffani Evelyn on 11/01/2017.
+//  Copyright © 2017 Tiffani Evelyn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
